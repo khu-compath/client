2a2c14149ba94f9c690106b4b24cc609fc5672e6
wingyou <winwing.you@gmail.com>
2024-07-23 19:51:49 +0900

    fix: remove unnessary imports & adjust default screen props

--- src/app/(app)/(tabs)/info-user.tsx
+++ src/app/(app)/(tabs)/info-user.tsx
@@ -1,7 +1,3 @@
----
-destinationDir: src/app/<%= props.subdirectory %>
-filename: <%= props.kebabCaseName %>.tsx
----
 import React from "react"
 import { observer } from "mobx-react-lite"
 import { ViewStyle } from "react-native"
@@ -9,13 +5,13 @@ import { Text, Screen } from "src/components"
 // import { spacing } from "src/theme"
 // import { useStores } from "src/models"
 
-export default observer(function <%= props.pascalCaseName %>Screen() {
+export default observer(function InfoUserScreen() {
   // Pull in one of our MST stores
   // const { someStore, anotherStore } = useStores()
 
   return (
     <Screen preset="fixed" style={$root} safeAreaEdges={["top"]}>
-      <Text text="<%= props.camelCaseName %>" />
+      <Text text="infoUser" />
     </Screen>
   )
 })
